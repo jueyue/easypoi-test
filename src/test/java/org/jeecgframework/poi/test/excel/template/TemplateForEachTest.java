a72c00e90921dca6b6728ffa9400007bee95fd87
jueyue <qrb.jueyue@gmail.com>
2016-11-29 13:43:38 +0800

    修改下 模板导出例子2

--- src/test/java/org/jeecgframework/poi/test/excel/template/TemplateForEachTest.java
+++ src/test/java/org/jeecgframework/poi/test/excel/template/TemplateForEachTest.java
@@ -40,15 +40,16 @@ public class TemplateForEachTest {
         }
         map.put("entitylist", list);
         map.put("letest", "12345678");
+        map.put("sexmark", "2");
         map.put("fntest", "12345678.2341234");
         map.put("fdtest", null);
         List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 3; i++) {
             Map<String, Object> testMap = new HashMap<String, Object>();
 
             testMap.put("id", "xman");
             testMap.put("name", "小明" + i);
-            testMap.put("sex", "man");
+            testMap.put("sex", "1");
             mapList.add(testMap);
         }
         map.put("maplist", mapList);
