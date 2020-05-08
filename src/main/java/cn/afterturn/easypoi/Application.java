855b28f5f6e7d8f20ac836540a79868671ebe996
jueyue <qrb.jueyue@gmail.com>
2020-05-08 22:07:07 +0800

    easypoi-wps模块,支持excel的预览以及编辑

--- src/main/java/cn/afterturn/easypoi/Application.java
+++ src/main/java/cn/afterturn/easypoi/Application.java
@@ -3,11 +3,17 @@ package cn.afterturn.easypoi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("easypoi-preview.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/easypoijs/**").addResourceLocations("classpath:/META-INF/resources/easypoijs/");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
